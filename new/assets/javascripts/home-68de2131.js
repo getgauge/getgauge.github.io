45f53b98621a55e3f74c8a2213090017055b55a7
sriv <srikanth.ddit@gmail.com>
2015-04-30 17:03:17 +0530

    added preview of new website

---
+++ new/assets/javascripts/home-68de2131.js
@@ -0,0 +1,1 @@
+var controller=new ScrollMagic.Controller;(new ScrollMagic.Scene).setPin(".section1").addTo(controller),new ScrollMagic.Scene({triggerElement:".section2",triggerHook:"onLeave"}).setPin(".section2").addTo(controller),new ScrollMagic.Scene({triggerElement:".section3",triggerHook:"onLeave"}).setPin(".section3").addTo(controller);
