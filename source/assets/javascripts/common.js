908edebcb6859f88d2ac4f9e1967c36a2235db63
sriv <srikanth.ddit@gmail.com>
2015-05-07 16:36:14 +0530

    moved features to homepage, some more styling changes

---
+++ source/assets/javascripts/common.js
@@ -0,0 +1,26 @@
+$(document).ready(function ($) {
+  $(window).stellar({
+    responsive: true,
+    horizontalScrolling: false
+  });
+
+  $('a.scroll-animate').click(function(){
+      $('html, body').animate({
+          scrollTop: $('[name="' + $.attr(this, 'href').substr(1) + '"]').offset().top
+      }, 500);
+      return false;
+  });
+});
+
+$(document).ready(function() {
+  var menu = $('.header-nav');
+  var menuToggle = $('.navigation-menu-button');
+
+  $(menuToggle).on('click', function(e) {
+    menu.slideToggle();
+  });
+
+  $(menu).on('click', function(e) {
+    menu.slideToggle();
+  });
+});
