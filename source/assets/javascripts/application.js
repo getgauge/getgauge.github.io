fef4148cd6644c702f2855832c5ce4a90424f6db
Kashishm <kashishmunjal64@gmail.com>
2015-09-15 16:34:07 +0530

    Highlighting platform according to OS present in user's machine in download page

--- source/assets/javascripts/application.js
+++ source/assets/javascripts/application.js
@@ -1,5 +1,4 @@
 // Do not write any JS here!
 
-//= require 'frameworks/jquery-1.11.2'
 //= require 'custom/jquery.stellar'
 //= require 'common'
---
+++ source/assets/javascripts/platform.js
@@ -0,0 +1,2 @@
+var highlight = {"border-width":"3px", "border-style":"solid"};
+$('#' + $.client.os).css(highlight).attr('title', 'Choose 32/64 bit version from this platform for your machine.');
