9ddd0533ff6b4b15b0d9b1a8e4753517bcf14e82
mahendrakariya <mahendrakariya@gmail.com>
2015-10-21 19:57:35 +0530

    pushing new redesign

--- new/assets/javascripts/platform.js
+++ new/assets/javascripts/platform.js
@@ -1,1 +1,1 @@
-var highlight={"border-width":"3px","border-style":"solid"};$("#"+$.client.os).css(highlight).attr("title","Choose 32/64 bit version from this platform for your machine.");
+window.onload=function(){$("#"+$.client.os+" a").click()};
