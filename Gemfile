913db655ef0e9f4e1875e952fe93fd890f77fac3
sriv <srikanth.ddit@gmail.com>
2017-02-24 16:05:10 +0530

    cleanups & upgrade gems
    - remove blogs
    - upgrade to ruby 2.4.0

--- Gemfile
+++ Gemfile
@@ -1,23 +1,13 @@
 source 'https://rubygems.org'
-#ruby 2.2.0
+#ruby 2.4.0
 
-gem 'middleman', '~> 3.4', '>= 3.4.1'
-gem 'middleman-blog'
-gem 'middleman-disqus'
+gem 'middleman'
 gem 'nokogiri'
 gem 'rake'
-
-# asset gems
-gem 'bourbon', '~> 4.2'
-gem 'neat'
-gem 'bitters'
 
 gem 'middleman-autoprefixer'
 gem 'bootstrap-sass'
 gem 'font-awesome-sass'
-
-# markdown & syntax
-gem "middleman-syntax"
 
 group :development do
   gem 'middleman-livereload'
--- config.rb
+++ config.rb
@@ -2,12 +2,10 @@ require 'active_support/core_ext/numeric/time'
 require 'bootstrap-sass'
 
 activate :automatic_image_sizes
-activate :directory_indexes
-activate :asset_host
 
 configure :development do
+  require 'middleman-livereload'
   activate :livereload
-  set :asset_host, "//localhost:4567"
 end
 
 set :fonts_dir, 'assets/fonts'
@@ -16,79 +14,29 @@ set :js_dir, 'assets/javascripts'
 set :images_dir, 'assets/images'
 set :layouts_dir, 'layouts'
 
-set :markdown, input: "GFM", :tables => true
 set :haml, { ugly: true }
 
-activate :syntax
-
 redirect 'download/index.html', to: '../get-started/index.html'
-
-activate :blog do |blog|
-  blog.prefix = "blog"
-  blog.sources = "posts/{year}-{month}-{day}-{title}.html"
-  blog.tag_template = "blog/tag.html"
-  blog.new_article_template = "source/blog/template.tt"
-  blog.default_extension = ".md"
-  blog.paginate = true
-  blog.page_link = "page-{num}"
-  blog.per_page = 6
-  blog.layout = "blog_layout"
-  blog.permalink = "{title}.html"
-  blog.taglink = "tags/{tag}.html"
-end
 
 helpers do
 	def is_home_page()
 		return current_page.url == '/'
 	end
 
-  def is_blog_article()
-    return current_page.url.start_with? "/blog/posts"
-  end
-
 	def nav_bar_link(text, url, opts={})
 		if current_page.url == '/'
 			link_to(text, url, opts)
-    elsif current_page.url.start_with? "/blog/posts"
-      link_to(text, '../../../../../' + url, opts)
 		else
 			link_to(text, '../' + url, opts)
 		end
 	end
-
-  def group_articles(articles)
-    g = {0 => [], 1 => [], 2 => []}
-    articles.each_with_index {|a,i| g[i%3] << a}
-    g
-  end
-
-  def filter_drafts(articles)
-    if is_preview then
-      articles
-    else
-      articles.select do | item |
-        !item.data.draft?
-      end
-    end
-  end
-
-  def is_preview
-    config[:environment] == :development
-  end
 end
 
-config[:file_watcher_ignore] += [ /.idea\// ]
+ignore '.idea'
 
 configure :build do
   set :relative_links, true
   activate :minify_css
   activate :minify_javascript
   activate :relative_assets
-
-  #abort "ENV['ASSET_HOST'] not specified, bailing!" if ENV['ASSET_HOST'].blank?
-  #set :asset_host, "//" + ENV['ASSET_HOST']
-end
-
-activate :disqus do |d|
-  d.shortname = 'getgauge'
 end
